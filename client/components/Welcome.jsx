c1bdf52f104a2c65d6708099da774a95d7a1198c
taty500summers <tatiana.bernon@gmail.com>
2020-09-04 08:17:25 +1200

    Add changes to repo

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -6,9 +6,9 @@ const Welcome = () => {
       <div>
         <div>
           <ul className="welcomeNav">
-            <li><a href="/">Home</a></li>
             <li><a href="/Login">Login</a></li>
             <li><a href="/About">About</a></li>
+            <li><a href="/Contacts">Contacts</a></li>
           </ul>
         </div>
         <h1 className='welcomeHeader'>Haere mai, Welcome to</h1>
--- client/index.js
+++ client/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import Welcome from './components/Welcome'
+import '../server/public/main.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
