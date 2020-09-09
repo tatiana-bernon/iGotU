38b46cc5a5013c6d6c6bc3803854f7312e62ca3c
taty500summers <tatiana.bernon@gmail.com>
2020-09-09 20:15:26 +1200

    Change style of forms

--- client/components/FormParent.jsx
+++ client/components/FormParent.jsx
@@ -9,19 +9,22 @@ const FormParent = () => {
       <div>
         <Navbar />
         <br />
+        <div className="has-text-centered">
+          <h1>Parent Profile Form</h1>
+          <h3>Kindly fill this form with the relevant information about you and your whānau.</h3>
+        </div>
         <fieldset>
-
           <div className="field">
             <label className="label">Name</label>
             <div className="control">
-              <input className="input" type="text" placeholder="e.g Alex Smith" />
+              <input className="input is-primary" type="text" placeholder="e.g Alex Smith" />
             </div>
           </div>
 
           <div className="field">
             <label className="label">Email</label>
             <div className="control">
-              <input className="input" type="email" placeholder="e.g. alexsmith@gmail.com" />
+              <input className="input is-primary" type="email" placeholder="e.g. alexsmith@gmail.com" />
             </div>
           </div>
 
--- client/components/FormTeacher.jsx
+++ client/components/FormTeacher.jsx
@@ -4,30 +4,63 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 
 const FormTeacher = () => {
+
   return (
+
     <>
+
       <div>
+
         <Navbar />
+
         <br />
+
+        <div className="has-text-centered">
+
+          <h1>Teacher Profile Form</h1>
+
+          <h3>Kindly fill this form with the relevant information about you and your teaching experience.</h3>
+
+        </div>
+
         <fieldset>
 
           <div className="field">
+
+            <br/>
+
+            <h5 className="has-text-primary-dark">Personal information</h5>
+
+            <br/>
+
             <label className="label">Name</label>
+
             <div className="control">
-              <input className="input" type="text" placeholder="e.g Alex Smith" />
+
+              <input className="input is-primary" type="text" placeholder="e.g Alex Smith" />
+
             </div>
+
           </div>
 
           <div className="field">
+
             <label className="label">Email</label>
+
             <div className="control">
-              <input className="input" type="email" placeholder="e.g. alexsmith@gmail.com" />
+
+              <input className="input is-primary" type="email" placeholder="e.g. alexsmith@gmail.com" />
+
             </div>
+
           </div>
 
         </fieldset>
+
         <br />
+
         <Footer />
+
       </div>
 
     </>
